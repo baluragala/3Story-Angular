817b8449f0bc0299748646bbb6295fb20fa463ef
Bala Krishna Ragala <baluragala@gmail.com>
2018-09-06 20:32:00 +0530

    setvalue,patchvalue and fb

--- ecourse/src/app/course/add-course-rf/add-course-rf.component.ts
+++ ecourse/src/app/course/add-course-rf/add-course-rf.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl, Validators } from "@angular/forms";
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from "@angular/forms";
 
 @Component({
   selector: "eco-add-course-rf",
@@ -10,23 +15,53 @@ export class AddCourseRfComponent implements OnInit {
   addCourseForm: FormGroup;
   isFormSubmitted: boolean = false;
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
-    this.addCourseForm = new FormGroup({
-      title: new FormControl("", [
-        Validators.required,
-        Validators.minLength(10),
-        Validators.maxLength(30)
-      ]),
-      price: new FormControl(),
-      modeOfDelivery: new FormControl(),
-      category: new FormControl()
+    // this.addCourseForm = new FormGroup({
+    //   title: new FormControl("", [
+    //     Validators.required,
+    //     Validators.minLength(10),
+    //     Validators.maxLength(30)
+    //   ]),
+    //   price: new FormControl(),
+    //   modeOfDelivery: new FormControl(),
+    //   category: new FormControl()
+    // });
+    // console.log(this.addCourseForm);
+
+    this.addCourseForm = this.fb.group({
+      title: [
+        "",
+        [
+          Validators.required,
+          Validators.minLength(10),
+          Validators.maxLength(30)
+        ]
+      ],
+      price: "",
+      modeOfDelivery: "",
+      category: ""
     });
-    console.log(this.addCourseForm);
   }
 
   onSubmit() {
     this.isFormSubmitted = true;
+  }
+
+  load() {
+    // this.addCourseForm.setValue({
+    //   title: "Angular 6 by zeol",
+    //   price: 100,
+    //   modeOfDelivery: "ONLINE",
+    //   category: "Mobile Development"
+    // });
+
+    this.addCourseForm.patchValue({
+      titl: "Angular 6 by zeol",
+      price: 100,
+      modeOfDelivery: "ONLINE",
+      category: "Mobile Development"
+    });
   }
 }
