2eeddcf7178ab1c399c4b84f339b91f060831193
Bala Krishna Ragala <baluragala@gmail.com>
2018-09-06 22:22:36 +0530

    router module

--- ecourse/src/app/app.module.ts
+++ ecourse/src/app/app.module.ts
@@ -6,10 +6,23 @@ import { HeaderComponent } from "./header.component";
 import { SharedModule } from "./shared/shared.module";
 import { CourseModule } from "./course/course.module";
 import { CourseService } from "./course/course.service";
+import { RouterModule } from "@angular/router";
+import { CourseListComponent } from "./course/course-list/course-list.component";
+import { AddCourseTdComponent } from "./course/add-course-td/add-course-td.component";
+import { AddCourseRfComponent } from "./course/add-course-rf/add-course-rf.component";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, SharedModule, CourseModule],
+  imports: [
+    BrowserModule,
+    SharedModule,
+    CourseModule,
+    RouterModule.forRoot([
+      { path: "list", component: CourseListComponent },
+      { path: "add-td", component: AddCourseTdComponent },
+      { path: "add-rf", component: AddCourseRfComponent }
+    ])
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
